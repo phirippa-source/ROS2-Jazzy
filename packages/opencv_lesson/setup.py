64f48005ec87d95d59dcbfcb8570cca1ca6673f9
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2025-11-14 13:04:55 +0900

    Update setup.py

--- packages/opencv_lesson/setup.py
+++ packages/opencv_lesson/setup.py
@@ -13,7 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', glob.glob(os.path.join('launch', '*.launch.*'))), 
-        ('share/' + package_name + '/launch', glob.glob(os.path.join('config', '*.yaml')))
+        ('share/' + package_name + '/config', glob.glob(os.path.join('config', '*.yaml')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
